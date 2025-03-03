7d7c123e0ab49cce7e07b8b87a4fe1bac45370b1
Ceeylla-Favv <bertleypriscy@gmail.com>
2025-03-03 13:41:58 +0100

    feat: get user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,7 +16,30 @@ const getUserById = async (req, res) => {
     
 };
 
+const getUserProfile = async (req, res) => {
+    try {
+        console.log("User ID from token:", req.user?.id);
+
+        if (!req.user || !req.user.id) {
+          return res
+            .status(401)
+            .json({ message: "Unauthorized: No user ID found" });
+        }
+
+        const user = await userModel.findById(req.user.id).select("-password");
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        return res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
 
 module.exports = {
-    getUserById
+    getUserById,
+    getUserProfile
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const { getUserById } = require("../controllers/userController");
+const { getUserById, getUserProfile } = require("../controllers/userController");
+const { isLoggedIn } = require("../middleware/authenticate");
 const router = express.Router();
 
+router.route("/profile").get(isLoggedIn, getUserProfile);
 router.route("/:id").get(getUserById);
+
 
 module.exports = router;
