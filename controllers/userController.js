03860037b1706a176d53076ec4836381d49dd1be
Ceeylla-Favv <bertleypriscy@gmail.com>
2025-03-03 14:18:00 +0100

    feat: add update on user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,8 +30,37 @@ const getUserProfile = async (req, res) => {
     }
 };
 
+const updateUserProfile = async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const updates = req.body;
+
+        const restrictedFields = [
+          "password",
+          "email",
+          "role",
+          "resetToken",
+          "resetExpires",
+        ];
+        restrictedFields.forEach((field) => delete updates[field]);
+
+        const updatedUser = await userModel.findByIdAndUpdate(userId, updates, {
+            new: true,
+            runValidators: true,
+        }).select("-password");
+
+        if (!updatedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        return res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
 
 module.exports = {
     getUserById,
-    getUserProfile
+    getUserProfile,
+    updateUserProfile
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { getUserById, getUserProfile } = require("../controllers/userController");
+const { getUserById, getUserProfile, updateUserProfile } = require("../controllers/userController");
 const { isLoggedIn } = require("../middleware/authenticate");
 const router = express.Router();
 
 router.route("/profile").get(isLoggedIn, getUserProfile);
 router.route("/:id").get(getUserById);
+router.route("/profile").patch(isLoggedIn, updateUserProfile);
 
 
 module.exports = router;
